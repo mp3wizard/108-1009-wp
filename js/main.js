718d930699e17c966c5762ea5f922816ea3e5c5d
Sappawish Siripon <mp3wizard@gmail.com>
2014-03-30 15:55:33 +0700

    - หน้า terms เรียบร้อย

--- js/main.js
+++ js/main.js
@@ -48,25 +48,6 @@ jQuery(document).ready(function($){
 		return false;
 	})
 
-	$('.popup').magnificPopup({
-		type:'image',
-		alignTop: false,
-		closeBtnInside: false,
-		image: {
-			verticalFit: true,
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<p><span>'+item.el.attr('data-view')+'</span> <a hreft="javascript:void(0);">'+item.el.attr('data-share')+' Stories</a><p>';
-			}
-		},
-		removalDelay: 500, //delay removal by X to allow out-animation
-  callbacks: {
-    beforeOpen: function() {
-      // just a hack that adds mfp-anim class to markup
-       this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-       this.st.mainClass = this.st.el.attr('data-effect');
-    }
-  }
-	});
 
 	// Disable click before login
 	$('.disable a').on('click touch', function() {
